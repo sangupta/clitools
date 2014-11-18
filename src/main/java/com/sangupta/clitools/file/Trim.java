ccda152457d4bfa512d325ee3033f507012049e9
Sandeep Gupta <sandy.pec@gmail.com>
2014-11-18 18:38:48 +0530

    Updated copyright and migrated to commons-lang3

--- src/main/java/com/sangupta/clitools/file/Trim.java
+++ src/main/java/com/sangupta/clitools/file/Trim.java
@@ -1,3 +1,24 @@
+/**
+ *
+ * clitools - Simple command line tools
+ * Copyright (c) 2014, Sandeep Gupta
+ * 
+ * http://sangupta.com/projects/clitools
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
 package com.sangupta.clitools.file;
 
 import io.airlift.command.Arguments;
@@ -13,10 +34,16 @@ import java.util.concurrent.atomic.AtomicLong;
 import javax.inject.Inject;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import com.sangupta.clitools.core.AbstractMultiFileTool;
 
+/**
+ * Remove all white spaces from start and end of each line in files
+ * 
+ * @author sangupta
+ *
+ */
 @Command(name = "trim", description = "Remove whitespaces from each line of the file")
 public class Trim extends AbstractMultiFileTool {
 	
