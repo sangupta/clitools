cbe61b51f480dff5dd0dc5050ba29eb8c20b970f
Sandeep Gupta <sandy.pec@gmail.com>
2015-03-28 00:36:52 +0530

    Added a tool to clean temporary folder of OS to recover space

---
+++ src/main/java/com/sangupta/clitools/dir/CleanTemp.java
@@ -0,0 +1,116 @@
+package com.sangupta.clitools.dir;
+
+import java.io.File;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import io.airlift.airline.Command;
+import io.airlift.airline.HelpOption;
+import io.airlift.airline.Option;
+import io.airlift.airline.SingleCommand;
+
+import com.sangupta.clitools.CliTool;
+import com.sangupta.jerry.constants.SystemPropertyNames;
+import com.sangupta.jerry.util.AssertUtils;
+import com.sangupta.jerry.util.FileUtils;
+import com.sangupta.jerry.util.ReadableUtils;
+
+/**
+ * Clean all temporary files from OS temporary folder.
+ * 
+ * @author sangupta
+ *
+ */
+@Command(name = "cleantmp", description = "Clean the current temp directories")
+public class CleanTemp implements CliTool {
+
+	@Inject
+	private HelpOption helpOption;
+	
+	@Option(name = { "--info", "-i" }, description = "Info mode, no cleaning is done")
+	private boolean info;
+	
+	@Option(name = { "--clean", "-c" }, description = "Clean mode, actually deletes files")
+	private boolean clean;
+	
+	public static void main(String[] args) {
+		if(AssertUtils.isEmpty(args)) {
+			args = new String[] { "--help" };
+		}
+		
+		CleanTemp cleanTemp = SingleCommand.singleCommand(CleanTemp.class).parse(args);
+		if(cleanTemp.helpOption.showHelpIfRequested()) {
+			return;
+		}
+		
+		if(cleanTemp.clean) {
+			cleanTemp.execute();
+			return;
+		}
+	}
+
+	private void execute() {
+		String tmpDir = System.getProperty(SystemPropertyNames.JAVA_TMPDIR);
+		File file = new File(tmpDir);
+		System.out.println("Temporary directory is " + file.getAbsoluteFile().getAbsolutePath());
+		
+		if(!file.exists()) {
+			System.out.println("Temporary directory does not exist.");
+			return;
+		}
+		
+		if(!file.isDirectory()) {
+			System.out.println("Temporary directory is not a directory.");
+			return;
+		}
+		
+		if(!file.canRead()) {
+			System.out.println("Unable to read temporary directory.");
+			return;
+		}
+		
+		List<File> files = FileUtils.listFiles(file.getAbsoluteFile().getAbsolutePath() + File.separator + "*", true);
+		if(AssertUtils.isEmpty(files)) {
+			System.out.println("Temporary directory has no files.");
+			return;
+		}
+		
+		if(this.info) {
+			showInfo(files);
+			return;
+		}
+		
+		recoverSpace(files);
+	}
+
+	private void recoverSpace(List<File> files) {
+		long size = 0;
+		int count = 0;
+		for(File file : files) {
+			if(file.exists() && file.isFile() && file.canWrite()) {
+				long fs = file.length();
+				boolean deleted = file.delete();
+				if(deleted) {
+					size += fs;
+					count++;
+				}
+			}
+		}
+		
+		System.out.println("Total recovered space is " + ReadableUtils.getReadableByteCount(size) + " from " + count + " files.");
+	}
+
+	private void showInfo(List<File> files) {
+		long size = 0;
+		int count = 0;
+		for(File file : files) {
+			if(file.exists() && file.isFile() && file.canWrite()) {
+				size += file.length();
+				count++;
+			}
+		}
+		
+		System.out.println("Total recoverable space is " + ReadableUtils.getReadableByteCount(size) + " in " + count + " files.");
+	}
+}
